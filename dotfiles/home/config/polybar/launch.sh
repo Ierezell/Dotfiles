3a98d9ddfcfae951d1278f220f0ede72b5064a85
Pierre Snell <ierezell@gmail.com>
2020-07-27 14:31:53 -0400

    Update

--- dotfiles/home/config/polybar/launch.sh
+++ dotfiles/home/config/polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
+# python ~/.config/polybar/scripts/spotify/launch_listener.py &
 polybar ArchBar &
 nm-applet &
 blueman-applet &
